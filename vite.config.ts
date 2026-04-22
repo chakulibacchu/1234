1fde335f9125fed22c2cb3f34817da77b86c526f
chakulibacchu <robb71253@gmail.com>
2026-04-22 18:24:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   preview: {
     port: PORT,
     host: true,
-    allowedHosts: ['one234-2893.onrender.com'], // <-- allow Render host
+    allowedHosts: ['https://one234-d1lj.onrender.com'], // <-- allow Render host
   },
   esbuild: {
     // ignore all TS errors
